f33e32114e24239414d8d7ca851443aacf43fe7f
nadeem977 <nadeemjaveid67@gmail.com>
2024-06-01 19:47:40 +0500

    updated

--- controllers/UserAuth.js
+++ controllers/UserAuth.js
@@ -3,37 +3,45 @@ const bcrypt = require("bcrypt");
 
 const addNewUser = async (req, res) => {
   try {
-    const criptpass = bcrypt.hashSync(req.body.password, 5);
-    //  const findUser = User.findOne({email:req.body.email})
-    //  if(findUser){
-    //   res.status(201).send("this email a")
-    //  }
-    const nnewUser = new User({
-      username: req.body.username,
-      email: req.body.email, 
-      phone: req.body.phone,
-      password: criptpass,
-    });
-    if (nnewUser) {
-      nnewUser.save();
-      res.status(200).send(nnewUser); 
+    const findUser = await User.findOne({ email: req.body.email });
+    if (findUser) {
+      return res.status(409).send("User with this email already exists");
     }
+
+    const cryptPass = bcrypt.hashSync(req.body.password, 5);
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      phone: req.body.phone,
+      password: cryptPass,
+    });
+
+    await newUser.save();
+    return res.status(201).send(newUser);
   } catch (error) {
     console.log(error);
-    res.status(500).send(error);   
-  } 
-};
-   
-const loginAuth = async (req, res) => {
-  try {
-    const user = await User.find({ email: req.body.email });
-    console.log(user)
-    if (!res) return res.status(404).send("this email is not exist ");
-    res.status(200).send(user);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
+    return res.status(500).send(error);
   }
 };
+
+const loginAuth = async (req, res) => {
+  try {
+    const findUser = await User.findOne({ email: req.body.email });
+    if (!findUser) {
+      return res.status(404).send("User with this email doesn't exist");
+    }
+
+    const isMatch = bcrypt.compareSync(req.body.password, findUser.password);
+    if (!isMatch) {
+      return res.status(404).send("Incorrect password");
+    }
+
+    return res.status(200).send(findUser);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+};
+
 
 module.exports = { addNewUser, loginAuth };
